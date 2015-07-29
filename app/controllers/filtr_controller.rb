62fdfbc1f49330b937a8e5e9b0a138d53c0dfb07
Dan <dangarbage15@yahoo.com>
2015-07-29 22:34:41 +0000

    finished styles and js for home page

--- app/controllers/filtr_controller.rb
+++ app/controllers/filtr_controller.rb
@@ -2,4 +2,8 @@ class FiltrController < ApplicationController
   def home
     render "home"
   end
+  
+  def creation_center
+    render "creationCenter"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "filtr#home"
+  
+  get "/creation_center" => "filtr#creation_center"
 end
